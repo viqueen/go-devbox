15dabf595bedf2ecd1c1fb824dc008a47b6cea48
viqueen <hasnae.rehioui@safetyculture.io>
2024-10-14 20:50:38 +1100

    implement scan go module

---
+++ internal/scan_tasks/scan_go_file.go
@@ -0,0 +1,8 @@
+package scan_tasks
+
+import "log"
+
+func ScanGoFile(file string) error {
+	log.Printf("Scanning file: %s\n", file)
+	return nil
+}
--- internal/scan_tasks/scn_go_module.go
+++ internal/scan_tasks/scn_go_module.go
@@ -1,8 +1,67 @@
 package scan_tasks
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
 
 func ScanGoModule(module string) error {
 	log.Printf("Scanning module: %s\n", module)
+	info, err := getModInfo(module)
+	if err != nil {
+		return err
+	}
+	goFiles, err := listGoFiles(info.Dir)
+	if err != nil {
+		return err
+	}
+	for _, file := range goFiles {
+		scanErr := ScanGoFile(file)
+		if scanErr != nil {
+			log.Printf("Error scanning file %s: %v\n", file, scanErr)
+		}
+	}
 	return nil
+}
+
+type modInfo struct {
+	Dir string `json:"Dir"`
+}
+
+func getModInfo(module string) (*modInfo, error) {
+	cmd := exec.Command("go", "list", "-json", module)
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var info modInfo
+	if jsonErr := json.Unmarshal(output, &info); jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return &info, nil
+}
+
+func listGoFiles(moduleDir string) ([]string, error) {
+	var goFiles []string
+	err := filepath.Walk(moduleDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if strings.HasSuffix(path, ".go") && !strings.HasSuffix(path, "_test.go") {
+			goFiles = append(goFiles, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return goFiles, nil
 }
