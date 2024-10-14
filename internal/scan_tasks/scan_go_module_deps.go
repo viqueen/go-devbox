bb194a22cb03394ecfac40270250abad28dee48d
viqueen <hasnae.rehioui@safetyculture.io>
2024-10-14 20:46:46 +1100

    implement goscan deps

--- internal/scan_tasks/scan_go_module_deps.go
+++ internal/scan_tasks/scan_go_module_deps.go
@@ -3,6 +3,8 @@ package scan_tasks
 import (
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func ScanGoModuleDeps() error {
@@ -11,5 +13,26 @@ func ScanGoModuleDeps() error {
 		return err
 	}
 	log.Printf("Scanning deps for: %s\n", cwd)
+	deps, err := listGoModuleDeps()
+	if err != nil {
+		return err
+	}
+	for _, dep := range deps {
+		parts := strings.Split(dep, " ")
+		if len(parts) < 2 {
+			continue
+		}
+		target := parts[0]
+		_ = ScanGoModule(target)
+	}
 	return nil
+}
+
+func listGoModuleDeps() ([]string, error) {
+	cmd := exec.Command("go", "list", "-m", "all")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(output), "\n"), nil
 }
